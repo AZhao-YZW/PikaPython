bc14c6b7a6423e8e5d20d91979d362c4b3b146a3
1milk11 <1114534452@qq.com>
2024-08-04 23:08:01 +0800

    commit

---
+++ test/hlt/test_case/list_dict_test/test_list_dict.py
@@ -0,0 +1,16 @@
+def test_list_dict():
+    l1 = list()
+    print("list l1:", l1) 
+
+    l2 = ['a', 'b', 'c']
+    print("List l2:", l2) 
+
+    d1 = dict()
+    print("dict d1:", d1)
+
+    d2 =  {'a':x, 'b':y, 'c':z}
+    print("dict d2:", d2)
+
+x, y, z = 10, 20, 30
+test_list_dict()
+
