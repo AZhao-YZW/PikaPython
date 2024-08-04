50dc83fcd74e378e3ae4a6090769765814238191
azhao <yuzhaowei2002@outlook.com>
2024-08-04 05:16:59 +0000

    [build] support build and test in REPL or FILE mode

--- build/run.sh
+++ build/run.sh
@@ -1,1 +1,19 @@
-./output/leafpython
+if [ ! -n "$1" ]; then
+    mode="REPL"
+    echo '===== default use REPL mode ====='
+elif [ "$1" = "REPL" ]; then
+    mode="REPL"
+    echo '===== use REPL mode ====='
+elif [ "$1" = "FILE" ]; then
+    mode="FILE"
+    echo '===== use FILE mode ====='
+else
+    echo '*ERROR*: mode is wrong, support value [REPL|FILE]'
+    exit 1
+fi
+
+if [ "$mode" = "REPL" ]; then
+    ./linux/repl/output/leafpython
+elif [ "$mode" = "FILE" ]; then
+    ./linux/file/output/leafpython
+fi
---
+++ test/Readme.md
@@ -0,0 +1,6 @@
+# HLT运行方式
+
+支持REPL模式和FILE模式的HLT测试：
+'''sh
+sh run_test.sh [REPL|FILE]
+'''
