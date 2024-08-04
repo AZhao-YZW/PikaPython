0dbaef95fc84dd17d37fa51fe3c3037d2f954fcf
AZhao <54131832+AZhao-YZW@users.noreply.github.com>
2024-08-04 20:06:26 +0800

    [test] add FILE mode test script frame (#11)

--- build/linux/file/src/main.c
+++ build/linux/file/src/main.c
@@ -3,10 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-    PikaObj* pikaMain = newRootObj("pikaMain", New_PikaMain);
-    pika_platform_printf("======[pikascript packages installed]======\r\n");
+    pika_platform_printf("======[pikapython packages installed]======\r\n");
     pika_printVersion();
+    pika_platform_printf("PikaStdLib===v1.13.3\r\n");
     pika_platform_printf("===========================================\r\n");
+
+    PikaObj* pikaMain = newRootObj("pikaMain", New_PikaMain);
     pikaVM_runFile(pikaMain, argv[1]);  // argv[1] is filename
     obj_deinit(pikaMain);
     return 0;
