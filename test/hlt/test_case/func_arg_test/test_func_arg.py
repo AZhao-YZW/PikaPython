19f068a1f43a010defc2a59590ab9140e0dd78e7
zyb <xmuzhangyuebin@gmail.com>
2024-08-08 21:00:43 +0800

    [test] func arg

---
+++ test/hlt/test_case/func_arg_test/test_func_arg.py
@@ -0,0 +1,18 @@
+def test_function_arguments(a, b=2, *args, **kwargs):
+    print("a:", a)
+    print("b:", b)
+    print("*args:", args)
+    print("**kwargs:", kwargs)
+
+print('======= Test Start =======')
+print("\nTest function: test_function_arguments(a, b=2, *args, **kwargs)")
+print("\nFunction arguments test:")
+test_function_arguments(1)
+test_function_arguments(1, 3)
+test_function_arguments(1, 3, 4, 5, 6)
+test_function_arguments(1, 3, 4, 5, 6, x=7, y=8)
+print("\nFunction arguments test with keyword arguments:")
+test_function_arguments(a=1)
+test_function_arguments(a=1, b=3)
+test_function_arguments(a=1, b=3, x=7, y=8)
+print('======= Test End =======')
