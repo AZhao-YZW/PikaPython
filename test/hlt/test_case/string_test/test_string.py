0824b1407697d0133b824d5246cfa9bd824b176f
zyb <xmuzhangyuebin@gmail.com>
2024-08-13 01:18:01 +0800

    [test] add string format and comparison operations test (#22)
    * [test] func arg
    
    * [test] add comparison and srting test

---
+++ test/hlt/test_case/string_test/test_string.py
@@ -0,0 +1,8 @@
+print('======= Test Start =======')
+
+str_test = "tests passed!"
+print('%%-formatting %s' %(str_test))
+print('str.format() {}'.format(str_test))
+print(f'f-strings (formatted string literals) {str_test}')
+
+print('======= Test End =======')
