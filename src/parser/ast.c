f3f602eaf5511f7b6c13af729d800691b9890964
AZhao <54131832+AZhao-YZW@users.noreply.github.com>
2024-08-29 00:41:19 +0800

    [parser] optimize parser_line_to_ast and add keyword_test (#42)

--- src/parser/ast.c
+++ src/parser/ast.c
@@ -40,102 +40,92 @@ extern PIKA_RES AST_parseSubStmt(AST *ast, char* sNodeContent);
 extern char* Cursor_popToken(Args* buffs, char** pStmt, char* devide);
 extern pika_bool Cursor_isContain(char* stmt, TokenType type, char* pyload);
 
-char* _defGetDefault(Args* outBuffs, char** sDeclearOut_p) {
+static char* get_def_default_stmt(Args* out_buffs, char** declare_out) {
 #if PIKA_NANO_ENABLE
     return "";
-#endif
+#else
     Args buffs = {0};
-    char* sDeclear = strsCopy(&buffs, *sDeclearOut_p);
-    char* sFnName = strsGetFirstToken(&buffs, sDeclear, '(');
-    Arg* aDeclear = arg_strAppend(arg_newStr(sFnName), "(");
-    Arg* aDefault = arg_newStr("");
-    char* sArgList = strsCut(&buffs, sDeclear, '(', ')');
-    char* sDefaultOut = NULL;
-    pika_assert(NULL != sArgList);
-    int iArgNum = _Cursor_count(sArgList, TOKEN_devider, ",", pika_true) + 1;
+    char* declare_str = strsCopy(&buffs, *declare_out);
+    char* fn_name = strsGetFirstToken(&buffs, declare_str, '(');
+    Arg* declare_arg = arg_strAppend(arg_newStr(fn_name), "(");
+    Arg* default_arg = arg_newStr("");
+    char* arg_list = strsCut(&buffs, declare_str, '(', ')');
+    char* default_out = NULL;
+    pika_assert(NULL != arg_list);
+    int iArgNum = _Cursor_count(arg_list, TOKEN_devider, ",", true) + 1;
     for (int i = 0; i < iArgNum; i++) {
-        char* sItem = Cursor_popToken(&buffs, &sArgList, ",");
-        char* sDefaultVal = NULL;
-        char* sDefaultKey = NULL;
-        pika_bool bDefault = 0;
-        if (Cursor_isContain(sItem, TOKEN_operator, "=")) {
+        char* item = Cursor_popToken(&buffs, &arg_list, ",");
+        char* default_val = NULL;
+        char* default_key = NULL;
+        bool is_default = false;
+        if (Cursor_isContain(item, TOKEN_operator, "=")) {
             /* has default value */
-            sDefaultVal = Cursor_splitCollect(&buffs, sItem, "=", 1);
-            sDefaultKey = Cursor_splitCollect(&buffs, sItem, "=", 0);
-            sDefaultKey = Cursor_splitCollect(&buffs, sDefaultKey, ":", 0);
-            aDefault = arg_strAppend(aDefault, sDefaultKey);
-            aDefault = arg_strAppend(aDefault, "=");
-            aDefault = arg_strAppend(aDefault, sDefaultVal);
-            aDefault = arg_strAppend(aDefault, ",");
-            bDefault = 1;
+            default_val = Cursor_splitCollect(&buffs, item, "=", 1);
+            default_key = Cursor_splitCollect(&buffs, item, "=", 0);
+            default_key = Cursor_splitCollect(&buffs, default_key, ":", 0);
+            default_arg = arg_strAppend(default_arg, default_key);
+            default_arg = arg_strAppend(default_arg, "=");
+            default_arg = arg_strAppend(default_arg, default_val);
+            default_arg = arg_strAppend(default_arg, ",");
+            is_default = true;
         } else {
-            sDefaultKey = sItem;
+            default_key = item;
         }
-        aDeclear = arg_strAppend(aDeclear, sDefaultKey);
-        if (bDefault) {
-            aDeclear = arg_strAppend(aDeclear, "=");
+        declare_arg = arg_strAppend(declare_arg, default_key);
+        if (is_default) {
+            declare_arg = arg_strAppend(declare_arg, "=");
         }
-        aDeclear = arg_strAppend(aDeclear, ",");
+        declare_arg = arg_strAppend(declare_arg, ",");
     }
-    strPopLastToken(arg_getStr(aDeclear), ',');
-    aDeclear = arg_strAppend(aDeclear, ")");
-    *sDeclearOut_p = strsCopy(outBuffs, arg_getStr(aDeclear));
-    sDefaultOut = strsCopy(outBuffs, arg_getStr(aDefault));
-    strPopLastToken(sDefaultOut, ',');
-    arg_deinit(aDeclear);
-    arg_deinit(aDefault);
+    strPopLastToken(arg_getStr(declare_arg), ',');
+    declare_arg = arg_strAppend(declare_arg, ")");
+    *declare_out = strsCopy(out_buffs, arg_getStr(declare_arg));
+    default_out = strsCopy(out_buffs, arg_getStr(default_arg));
+    strPopLastToken(default_out, ',');
+    arg_deinit(declare_arg);
+    arg_deinit(default_arg);
     strsDeinit(&buffs);
-    return sDefaultOut;
+    return default_out;
+#endif
 }
 
 /**
  * parser_line_to_ast
  */
-static uint8_t normal_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs,
-                                char *keyword)
+static uint8_t expr_kw_to_ast(char *kw, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
 {
-    *stmt = strsCut(buffs, line_start, ' ', ':');
-    AST_setNodeBlock(ast, keyword);
-    stack_pushStr(blk_state->stack, keyword);
+    *stmt = strsCut(buffs, *stmt, ' ', ':');
+    AST_setNodeBlock(ast, kw);
+    stack_pushStr(blk_state->stack, kw);
     return BLK_MATCHED;
 }
 
-uint8_t while_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
+static uint8_t no_expr_kw_to_ast(char *kw, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
 {
-    return normal_kw_to_ast(line_start, stmt, ast, blk_state, buffs, "while");
-}
-
-uint8_t if_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
-{
-    return normal_kw_to_ast(line_start, stmt, ast, blk_state, buffs, "if");
-}
-
-uint8_t elif_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
-{
-    return normal_kw_to_ast(line_start, stmt, ast, blk_state, buffs, "elif");
-}
-
-static uint8_t control_kw_to_ast(char **stmt, AST *ast, char *keyword)
-{
-    AST_setNodeAttr(ast, keyword, "");
-    *stmt = "";
+    uint8_t kw_len = strlen(kw);
+    if ((*stmt)[kw_len] == ':' || strsCut(buffs, *stmt, ' ', ':') != NULL) {
+        AST_setNodeBlock(ast, kw);
+        stack_pushStr(blk_state->stack, kw);
+        *stmt = "";
+    }
     return BLK_MATCHED;
 }
 
-uint8_t break_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
+static uint8_t control_kw_to_ast(char *kw, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
 {
-    return control_kw_to_ast(stmt, ast, "break");
+    AST_setNodeAttr(ast, kw, "");
+    if (strcmp(kw, "raise") == 0) {
+        *stmt = "RuntimeError";
+    } else {
+        *stmt = "";
+    }
+    return BLK_MATCHED;
 }
 
-uint8_t continue_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
-{
-    return control_kw_to_ast(stmt, ast, "continue");
-}
-
-uint8_t for_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
+uint8_t for_kw_to_ast(char *kw, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
 {
     Args* list_buffs = New_strBuff();
-    char* line_buff = strsCopy(list_buffs, line_start + 4);
+    char* line_buff = strsCopy(list_buffs, *stmt + 4);
     line_buff = Cursor_getCleanStmt(list_buffs, line_buff);
     if (strCountSign(line_buff, ':') < 1) {
         args_deinit(list_buffs);
@@ -156,39 +146,9 @@ uint8_t for_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_s
     return BLK_MATCHED;
 }
 
-uint8_t else_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
+uint8_t return_kw_to_ast(char *kw, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
 {
-    if ((line_start[4] == ' ') || (line_start[4] == ':')) {
-        *stmt = "";
-        AST_setNodeBlock(ast, "else");
-        stack_pushStr(blk_state->stack, "else");
-    }
-    return BLK_MATCHED;
-}
-
-uint8_t try_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
-{
-    if ((line_start[3] == ' ') || (line_start[3] == ':')) {
-        *stmt = "";
-        AST_setNodeBlock(ast, "try");
-        stack_pushStr(blk_state->stack, "try");
-    }
-    return BLK_MATCHED;
-}
-
-uint8_t except_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
-{
-    if ((line_start[6] == ' ') || (line_start[6] == ':')) {
-        *stmt = "";
-        AST_setNodeBlock(ast, "except");
-        stack_pushStr(blk_state->stack, "except");
-    }
-    return BLK_MATCHED;
-}
-
-uint8_t return_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
-{
-    char *line_buff = strsCopy(buffs, line_start);
+    char *line_buff = strsCopy(buffs, *stmt);
     strsPopToken(buffs, &line_buff, ' ');
     *stmt = line_buff;
     *stmt = Suger_multiReturn(buffs, *stmt);
@@ -196,17 +156,10 @@ uint8_t return_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *bl
     return BLK_MATCHED;
 }
 
-uint8_t return_eq_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
+uint8_t raise_kw_to_ast(char *kw, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
 {
-    AST_setNodeAttr(ast, "return", "");
-    *stmt = "";
-    return BLK_MATCHED;
-}
-
-uint8_t raise_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
-{
+    char* line_buff = strsCopy(buffs, *stmt);
     AST_setNodeAttr(ast, "raise", "");
-    char* line_buff = strsCopy(buffs, line_start);
     strsPopToken(buffs, &line_buff, ' ');
     *stmt = line_buff;
     if (strEqu("", *stmt)) {
@@ -215,18 +168,10 @@ uint8_t raise_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk
     return BLK_MATCHED;
 }
 
-uint8_t raise_eq_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
+uint8_t assert_kw_to_ast(char *kw, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
 {
-    AST_setNodeAttr(ast, "raise", "");
-    *stmt = "RuntimeError";
-    return BLK_MATCHED;
-}
-
-uint8_t assert_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
-{
-    *stmt = "";
+    char* line_buff = strsCopy(buffs, *stmt + 7);
     AST_setNodeAttr(ast, "assert", "");
-    char* line_buff = strsCopy(buffs, line_start + 7);
     /* assert expr [, msg] */
     while (1) {
         char* sub_stmt = Parser_popSubStmt(buffs, &line_buff, ",");
@@ -235,33 +180,34 @@ uint8_t assert_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *bl
             break;
         }
     }
+    *stmt = "";
     return BLK_MATCHED;
 }
 
-uint8_t global_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
+uint8_t global_kw_to_ast(char *kw, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
 {
-    *stmt = "";
-    char* global_list = line_start + 7;
+    char* global_list = *stmt + 7;
     global_list = Cursor_getCleanStmt(buffs, global_list);
     AST_setNodeAttr(ast, "global", global_list);
+    *stmt = "";
     return BLK_MATCHED;
 }
 
-uint8_t del_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
+uint8_t del_kw_to_ast(char *kw, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
 {
-    *stmt = "";
-    char* del_dir = strsCut(buffs, line_start, '(', ')');
+    char* del_dir = strsCut(buffs, *stmt, '(', ')');
     if (!del_dir) {
-        del_dir = line_start + sizeof("del ") - 1;
+        del_dir = *stmt + sizeof("del ") - 1;
     }
     del_dir = Cursor_getCleanStmt(buffs, del_dir);
     AST_setNodeAttr(ast, "del", del_dir);
+    *stmt = "";
     return BLK_MATCHED;
 }
 
-uint8_t def_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
+uint8_t def_kw_to_ast(char *kw, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
 {
-    char* declare = strsCut(buffs, line_start, ' ', ':');
+    char* declare = strsCut(buffs, *stmt, ' ', ':');
     *stmt = "";
     if (NULL == declare) {
         return BLK_EXIT;
@@ -271,7 +217,7 @@ uint8_t def_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_s
     if (!strIsContain(declare, '(') || !strIsContain(declare, ')')) {
         return BLK_EXIT;
     }
-    char* default_stmt = _defGetDefault(buffs, &declare);
+    char* default_stmt = get_def_default_stmt(buffs, &declare);
     AST_setNodeBlock(ast, "def");
     AST_setNodeAttr(ast, "declare", declare);
     if (default_stmt[0] != '\0') {
@@ -281,9 +227,9 @@ uint8_t def_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_s
     return BLK_MATCHED;
 }
 
-uint8_t class_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
+uint8_t class_kw_to_ast(char *kw, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
 {
-    char* declare = strsCut(buffs, line_start, ' ', ':');
+    char* declare = strsCut(buffs, *stmt, ' ', ':');
     *stmt = "";
     if (NULL == declare) {
         return BLK_EXIT;
@@ -295,46 +241,53 @@ uint8_t class_kw_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk
     return BLK_MATCHED;
 }
 
-uint8_t keyword_to_ast(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
+uint8_t keyword_to_ast(char **stmt, AST *ast, BlockState *blk_state, Args *buffs)
 {
     static struct {
         char *kw;
         uint8_t match_type;
-        uint8_t (*kw_to_ast)(char *line_start, char **stmt, AST *ast, BlockState *blk_state, Args *buffs);
+        uint8_t (*kw_to_ast)(char *kw, char **stmt, AST *ast, BlockState *blk_state, Args *buffs);
     } kw_map[] = {
-        { "while ",    KW_START, while_kw_to_ast     },
-        { "if ",       KW_START, if_kw_to_ast        },
-        { "elif ",     KW_START, elif_kw_to_ast      },
-        { "break",     KW_EQUAL, break_kw_to_ast     },
-        { "break ",    KW_START, break_kw_to_ast     },
-        { "continue",  KW_EQUAL, continue_kw_to_ast  },
-        { "continue ", KW_START, continue_kw_to_ast  },
-        { "for ",      KW_START, for_kw_to_ast       },
-        { "else",      KW_START, else_kw_to_ast      },
-        { "return",    KW_EQUAL, return_eq_kw_to_ast },
+        { "while ",    KW_START, expr_kw_to_ast      },
+        { "if ",       KW_START, expr_kw_to_ast      },
+        { "elif ",     KW_START, expr_kw_to_ast      },
+        { "else",      KW_START, no_expr_kw_to_ast   },
+        { "break",     KW_EQUAL, control_kw_to_ast   },
+        { "break ",    KW_START, control_kw_to_ast   },
+        { "continue",  KW_EQUAL, control_kw_to_ast   },
+        { "continue ", KW_START, control_kw_to_ast   },
+        { "return",    KW_EQUAL, control_kw_to_ast   },
         { "return ",   KW_START, return_kw_to_ast    },
-    #if PIKA_SYNTAX_EXCEPTION_ENABLE
-        { "try",       KW_START, try_kw_to_ast       },
-        { "except",    KW_START, except_kw_to_ast    },
-        { "raise",     KW_EQUAL, raise_eq_kw_to_ast  },
-        { "raise ",    KW_START, raise_kw_to_ast     },
-        { "assert ",   KW_START, assert_kw_to_ast    },
-    #endif
+        { "for ",      KW_START, for_kw_to_ast       },
         { "global ",   KW_START, global_kw_to_ast    },
         { "del ",      KW_START, del_kw_to_ast       },
         { "del(",      KW_START, del_kw_to_ast       },
         { "def ",      KW_START, def_kw_to_ast       },
         { "class ",    KW_START, class_kw_to_ast     },
+    #if PIKA_SYNTAX_EXCEPTION_ENABLE
+        { "try",       KW_START, no_expr_kw_to_ast   },
+        { "except",    KW_START, no_expr_kw_to_ast   },
+        { "raise",     KW_EQUAL, control_kw_to_ast   },
+        { "raise ",    KW_START, raise_kw_to_ast     },
+        { "assert ",   KW_START, assert_kw_to_ast    },
+    #endif
     };
+    char kw[15] = {0};
+    uint8_t kw_len;
 
     for (uint8_t i = 0; i < sizeof(kw_map) / sizeof(kw_map[0]); i++) {
-        if (kw_map[i].match_type == KW_START && !strIsStartWith(line_start, kw_map[i].kw)) {
+        if (kw_map[i].match_type == KW_START && !strIsStartWith(*stmt, kw_map[i].kw)) {
             continue;
         }
-        if (kw_map[i].match_type == KW_EQUAL && !strEqu(line_start, kw_map[i].kw)) {
+        if (kw_map[i].match_type == KW_EQUAL && !strEqu(*stmt, kw_map[i].kw)) {
             continue;
         }
-        return kw_map[i].kw_to_ast(line_start, stmt, ast, blk_state, buffs);
+        kw_len = strlen(kw_map[i].kw);
+        strncpy(kw, kw_map[i].kw, sizeof(kw));
+        if (kw[kw_len - 1] == ' ') {
+            kw[kw_len - 1] = '\0';
+        }
+        return kw_map[i].kw_to_ast(kw, stmt, ast, blk_state, buffs);
     }
     return BLK_NOT_MATCHED;
 }
@@ -345,7 +298,7 @@ AST* parser_line_to_ast(Parser* self, char* line)
     AST *ast = AST_create();
     int8_t blk_depth_now, blk_depth_last = -1;
     Args buffs = {0};
-    char *line_start, *stmt;
+    char *stmt;
     uint8_t ret;
 
     /* line is not exist */
@@ -398,10 +351,9 @@ AST* parser_line_to_ast(Parser* self, char* line)
         }
     }
 
-    line_start = line + (blk_depth_now - blk_state->depth) * 4;
-    stmt = line_start;
+    stmt = line + (blk_depth_now - blk_state->depth) * 4;
 
-    ret = keyword_to_ast(line_start, &stmt, ast, blk_state, &buffs);
+    ret = keyword_to_ast(&stmt, ast, blk_state, &buffs);
     if (ret == BLK_MATCHED) {
         goto __block_matched;
     } else if (ret == BLK_EXIT) {
