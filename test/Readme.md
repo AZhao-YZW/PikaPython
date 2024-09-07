903ad1ed14fbc3781dca44849c1e3729f5ceb493
azhao <yuzhaowei2002@outlook.com>
2024-09-07 13:32:42 +0000

    [test] fix run_benchmark.sh

--- test/Readme.md
+++ test/Readme.md
@@ -1,6 +1,6 @@
 # How to use:
 '''sh
-sh run_test.sh [REPL|FILE|ALL] [CPYTHON]
+sh run_test.sh [REPL|FILE|ALL] [LEAF|PIKA|CPYTHON]
 '''
 param 1: mode
 param 2: interpreter
--- test/benchmark/run_benchmark.sh
+++ test/benchmark/run_benchmark.sh
@@ -1,7 +1,32 @@
+#-------------------- HELP ---------------------#
+if [ "$1" = "-h" ]; then
+    echo 'FORMAT: sh run_benchmark.sh [LEAF|PIKA]'
+    exit 0
+fi
+
+#----------------- SELECT CORE -----------------#
+if [ ! -n "$1" ]; then
+    core="LEAF"
+    echo '===== default use LEAF core ====='
+elif [ "$1" = "LEAF" ] || [ "$1" = "PIKA" ]; then
+    core="$1"
+    echo "===== use $1 core ====="
+else
+    echo '*ERROR*: core is wrong, support value [LEAF|PIKA]'
+    exit 0
+fi
+
+#------------------ RUN BENCH ------------------#
+if [ "$core" = "LEAF" ]; then
+    output_dir=leaf
+elif [ "$core" = "PIKA" ]; then
+    output_dir=pika
+fi
+
 pika_dir=../../build/linux/pikapython
 file_dir=../../build/linux/file
 benchmark_dir=.
-cp ${file_dir}/output/leafpython ${pika_dir}
+cp ${file_dir}/output/$output_dir/leafpython ${pika_dir}
 mv ${pika_dir}/main.py ${pika_dir}/main.py.bak
 cp ${benchmark_dir}/mem_test.py ${pika_dir}/main.py
 ./${pika_dir}/leafpython ${pika_dir}/main.py
